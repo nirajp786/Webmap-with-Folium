0abb4cf729222cf1a34e01ac9e1750b1319bcfac
Niraj Patel <786nirajpatel@gmail.com>
2020-06-27 21:41:02 -0400

    Read data from volcanoes.txt

--- map.py
+++ map.py
@@ -1,8 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green', icon_color='white')))
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='red', icon_color='white')))
 
 map.add_child(fg)
+
 map.save("Map.html")
