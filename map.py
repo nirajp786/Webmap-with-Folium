fd5eab6e1ca4db38e4452fe5c0149a10a4beb422
Niraj Patel <786nirajpatel@gmail.com>
2020-06-28 18:43:08 -0400

    Added GeoJson data

--- map.py
+++ map.py
@@ -1,16 +1,36 @@
 import folium
 import pandas
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000<= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+<h4>Volcano information:</h4>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='red', icon_color='white')))
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, str(el)), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+    #fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el), icon_color='white')))
+
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), 
+                            style_function=lambda x: {'fillColor': 'yellow'}))
 
 map.add_child(fg)
 
